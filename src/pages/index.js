692a1ca1494269fbd886eb1d652719377327aa28
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-09-09 15:44:56 -0400

    added contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
+import PhoneIcon from '@mui/icons-material/Phone';
 
 const IndexPage = ({ data }) => {
 
@@ -27,7 +28,20 @@ const IndexPage = ({ data }) => {
       </div>
 
       <div id="contact">
-        Contact Us Section
+        <h1 class="contactHeader">Contact Us</h1>
+
+        <a href="tel:5854812707" class="contactPhone">
+          <button class="contactButton">
+            <PhoneIcon color="white" sx={{marginRight: "15px"}} class="phoneIcon" />
+            (585) 481-2707
+          </button>
+        </a>
+
+        <p style={{textAlign: "center", marginBottom: "15px"}}>3259 S Winton Rd, Rochester, NY 14623</p>
+        <div class="mapWrapper">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11654.999430528336!2d-77.58275!3d43.088754!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d14a937292d7e3%3A0xac98dda34cc188f!2sBlue%20Rock%20Liquor%2C%20Wine%20%26%20Spirits!5e0!3m2!1sen!2sus!4v1725910098914!5m2!1sen!2sus" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+        </div>
       </div>
     </Layout>
   )
