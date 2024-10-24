15658357ce7c4be791fb89bebfa83b6ff5986758
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2024-10-24 15:40:54 -0400

    logged wines

--- src/pages/{strapiCategory.slug}.js
+++ src/pages/{strapiCategory.slug}.js
@@ -6,39 +6,40 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 const Category = ({ data }) => {
 
+  console.log(data)
 
-    return (
-            <Layout pageTitle={data.strapiCategory.name}>
-        <ul class="postlist">
-            {
-                data.strapiCategory.wines.map(wine => (
-                    <li key={wine.id}>
-                        <Link class="winelink" to={`/${wine.slug}`}><h3>{wine.title}</h3></Link>
-                        <div class="image-wrap">
-                        <GatsbyImage class="cover" image={wine.image.localFile.childImageSharp.gatsbyImageData} alt={`Cover for ${wine.title}`} />
-                        </div>
-                        <p class="date">{wine.date}</p>
-                        <p class="description">{wine.description}</p>
-                    </li>
-                )
-                )
-            }
-               {
-                data.strapiCategory.liquors.map(liquor => (
-                    <li key={liquor.id}>
-                        <Link class="winelink" to={`/${liquor.slug}`}><h3>{liquor.title}</h3></Link>
-                        <div class="image-wrap">
-                        <GatsbyImage class="cover" image={liquor.image.localFile.childImageSharp.gatsbyImageData} alt={`Cover for ${liquor.title}`} />
-                        </div>
-                        <p class="date">{liquor.date}</p>
-                        <p class="description">{liquor.description}</p>
-                    </li>
-                )
-                )
-            }
-            </ul>
-            </Layout>
-    )
+  return (
+    <Layout pageTitle={data.strapiCategory.name}>
+      <ul class="postlist">
+        {
+          data.strapiCategory.wines.map(wine => (
+            <li key={wine.id}>
+              <Link class="winelink" to={`/${wine.slug}`}><h3>{wine.title}</h3></Link>
+              <div class="image-wrap">
+                <GatsbyImage class="cover" image={wine.image.localFile.childImageSharp.gatsbyImageData} alt={`Cover for ${wine.title}`} />
+              </div>
+              <p class="date">{wine.date}</p>
+              <p class="description">{wine.description}</p>
+            </li>
+          )
+          )
+        }
+        {
+          data.strapiCategory.liquors.map(liquor => (
+            <li key={liquor.id}>
+              <Link class="winelink" to={`/${liquor.slug}`}><h3>{liquor.title}</h3></Link>
+              <div class="image-wrap">
+                <GatsbyImage class="cover" image={liquor.image.localFile.childImageSharp.gatsbyImageData} alt={`Cover for ${liquor.title}`} />
+              </div>
+              <p class="date">{liquor.date}</p>
+              <p class="description">{liquor.description}</p>
+            </li>
+          )
+          )
+        }
+      </ul>
+    </Layout>
+  )
 }
 
 export const query = graphql`
